99d99d83a1f1bd642a8ae262733c0d5207127a18
NZ-Marty92 <martin_gatens@outlook.com>
2025-07-17 11:16:57 +1200

    Update Hero.tsx
    Updated hero to Logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { ArrowDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import heroBg from "@/assets/hero-bg.jpg";
+import heroBg from "@/assets/Logo.png";
 
 const Hero = () => {
   const scrollToAbout = () => {
